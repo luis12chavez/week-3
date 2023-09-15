890fa3f69bae6f5ee41175c359038071cc797a40
Luis Chavez <luis12chavez@gmail.com>
2023-09-15 12:07:38 -0700

    dw

--- playground/server.py
+++ playground/server.py
@@ -3,15 +3,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return "main page"
+    return "++main page++"
+
+# @app.route('/play_old')
+# def play():
+#     return render_template("index.html")
 
 @app.route('/play')
 def play():
-    return render_template("index.html")
+    return render_template("main_index.html", num = 3, color = "aqua")
 
 @app.route('/play/<int:num>')
 def play_times(num):
-    return render_template("index.html", num = num)
+    return render_template("main_index.html", num = num, color = "aqua")
+
+@app.route('/play/<int:num>/<string:color>')
+def play_color(num, color):
+    return render_template("main_index.html", num = num, color = color)
 
 if __name__ == "__main__":
     app.run(debug=True)
